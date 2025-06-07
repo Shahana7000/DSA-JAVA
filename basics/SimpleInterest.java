76122912861fe3e8df56eb1585402470895c0764
Shahana700 <shahanacs2003@gmail.com>
2025-06-07 17:43:16 +0530

    add new file

---
+++ basics/SimpleInterest.java
@@ -0,0 +1,9 @@
+public class SimpleInterest {
+    public static void main(String args[]){
+        int principal = 1000;
+        int rate = 10;
+        int time = 5;
+        int si = principal*rate*time/100;
+        System.out.println("simple interest is :"+si);
+    }
+}
