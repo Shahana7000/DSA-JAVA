f52ea67e985a5159ca680dc9cce6dabaf493c6f6
Shahana700 <shahanacs2003@gmail.com>
2025-06-13 23:06:45 +0530

    add a factorial file

---
+++ basics/Factorial.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String args[]){
+        Scanner cs = new Scanner(System.in);
+        int n = cs.nextInt();
+        int fact = 1;
+        for(int i =0)
+    }
+    
+}
