a51b76075b2bb63abd604b61aa7ceabcdcc5c7c3
Shahana700 <shahanacs2003@gmail.com>
2025-06-14 23:59:36 +0530

    few changes on this file

--- basics/PrimeCount.java
+++ basics/PrimeCount.java
@@ -1,27 +1,28 @@
-public class PrimeCount{
-   public static void main(String args[]) {
+public class PrimeCount {
+
+  
+    public static void main(String[] args) {
+        PrimeCount pc = new PrimeCount();
+        int n = 10; 
+        int result = pc.countPrimes(n);
+        System.out.println("Number of primes less than " + n + " is: " + result);
+    }
+
+    public int countPrimes(int n) {
         int counter = 0;
 
-        for(int i = 2;i<n;i++){
-            if(isPrime(i)){
+        for (int i = 2; i < n; i++) {
+            if (isPrime(i)) {
                 counter++;
-
             }
         }
-       return counter;
+        return counter;
     }
-    public boolean isPrime(int num){
-        for(int i = 2;i*i<=num;i++){
-            if(num % i ==0)return false;
+
+    public boolean isPrime(int num) {
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
         }
         return true;
     }
 }
-
-
-    
-
-            
-        
-    
-            
