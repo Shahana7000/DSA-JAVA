97bb25e9f0389f369ca39de0c53ce1c258e54840
Shahana700 <shahanacs2003@gmail.com>
2025-06-17 00:13:59 +0530

    add new file MajorityElement

---
+++ Array/MajorityElement.java
@@ -0,0 +1,23 @@
+public class MajorityElement {
+    public static int majorityElement(int[] nums){
+        int count = 0;
+        int candidate = 0;
+        for(int num : nums){
+            if(count == 0){
+                candidate = num;
+        }
+        if(candidate == num){
+            count ++;
+        }
+        else{
+            count --;
+        }
+    }
+    return candidate;
+    }
+public static void main(String[] args) {
+    int[] nums = {3,2,3,2,2};
+    System.out.println(majorityElement(nums));
+}
+}
+
