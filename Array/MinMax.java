d397cb333c8837fe0011c5af85d22cde013c30e0
Shahana700 <shahanacs2003@gmail.com>
2025-06-19 20:10:08 +0530

    only small changes

--- Array/MinMax.java
+++ Array/MinMax.java
@@ -1,15 +1,15 @@
 public class MinMax {
     public static void main(String args[]){
-        int[] nums = {23, 33,44,67,23,43,89};
-        int max = nums[0];
-        int min = nums[0];
+        int[] numss = {23, 33,44,67,23,43,89};
+        int max = numss[0];
+        int min = numss[0];
 
-        for(int i = 0;i<nums.length;i++){
-            if(nums[i] > max){
-                max = nums[i];
+        for(int i = 0;i<numss.length;i++){
+            if(numss[i] > max){
+                max = numss[i];
             }
-            if(nums[i] < min){
-                min = nums[i];
+            if(numss[i] < min){
+                min = numss[i];
             }
 
         }
