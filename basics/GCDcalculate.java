234800a348858a2a5bc925af095011376330e600
Shahana700 <shahanacs2003@gmail.com>
2025-06-20 23:31:24 +0530

    some changes

--- basics/GCDcalculate.java
+++ basics/GCDcalculate.java
@@ -15,10 +15,10 @@ public class GCDcalculate {
 
         sc.close(); 
     }
-public static int calculateGCD(int num1, int num2) {
-        while (num2 != 0) {
-            int temp = num2;
-            num2 = num1 % num2;
+public static int calculateGCD(int num1, int numm2) {
+        while (numm2 != 0) {
+            int temp = numm2;
+            numm2 = num1 % numm2;
             num1 = temp;
         }
         return num1;
