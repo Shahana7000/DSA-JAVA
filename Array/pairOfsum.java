95fa47e52be80cfae607d15e750840ace2e4559f
Shahana700 <shahanacs2003@gmail.com>
2025-06-25 22:30:52 +0530

    just changing input

--- Array/pairOfsum.java
+++ Array/pairOfsum.java
@@ -1,6 +1,6 @@
 public class pairOfsum {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5,6,7};
+        int[] arr = {4,5,6,7,8,9};
         int target = 7;
         int i = 0;
         int j = arr.length - 1;
