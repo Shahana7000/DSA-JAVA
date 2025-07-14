5704ff6a2dab12897d211c1f78b3ef5ce93a8daa
Shahana700 <shahanacs2003@gmail.com>
2025-07-14 21:39:25 +0530

    add something

--- patterns/pattern1.java
+++ patterns/pattern1.java
@@ -6,6 +6,13 @@ public class pattern1 {
             }
             System.out.println();
         }
+        for(int i = n-1;i>=1;i--){
+            for(int j = 1;j<=i;j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+
+        }
         return n;
     }
     public static void main(String args[]){
