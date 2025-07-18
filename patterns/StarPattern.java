9f8f2471cded90f8259e75ddd7fd54be0482ebad
Shahana700 <shahanacs2003@gmail.com>
2025-07-18 23:23:31 +0530

    pyramid pattern

--- patterns/StarPattern.java
+++ patterns/StarPattern.java
@@ -1,18 +1,33 @@
+// public class StarPattern {
+//     public static void main(String[] args) {
+//         int n = 5;
+//         for(int i = 1;i<=n;i++){
+//             for(int j = 1;j<=i*2-1;j++){
+//                 System.out.print(" *");
+//             }
+//             System.out.println();
+//         }
+//         for(int i = 1;i<=n-1;i++){
+//             for(int j = 1;j<=2*(n-i)-1;j++){
+//                 System.out.print(" *");
+    
+// }
+// System.out.println();
+// }
+//     }
+// }
 public class StarPattern {
     public static void main(String[] args) {
         int n = 5;
         for(int i = 1;i<=n;i++){
-            for(int j = 1;j<=i*2-1;j++){
-                System.out.print(" *");
+            for(int j = 1;j<n-1;j++){
+                System.out.print(" ");
             }
-            System.out.println();
+            for(int k = 1;k<=i*2-1;k++){
+                System.out.print("*");
         }
-        for(int i = 1;i<=n-1;i++){
-            for(int j = 1;j<=2*(n-i)-1;j++){
-                System.out.print(" *");
-    
-}
-System.out.println();
+        System.out.println();
 }
     }
 }
+
