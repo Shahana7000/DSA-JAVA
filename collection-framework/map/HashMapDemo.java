01f8badbceb4663b516e46607a4f97c1aa8a9635
Shahana700 <shahanacs2003@gmail.com>
2025-07-19 22:34:13 +0530

    collection framework

--- collection-framework/map/HashMapDemo.java
+++ collection-framework/map/HashMapDemo.java
@@ -1,12 +1,17 @@
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.Map.Entry;
 import java.util.HashMap;
 public class HashMapDemo {
     public static void main(String args[]){
-        Map<String, Integer> map = new HashMap<String, Integer>();
+        Map<String, Integer> map = new TreeMap<String, Integer>();
         map.put("shahana",22);
         map.put("saksham", 20);
         map.put("alfi", 15);
+        map.put("siddharth", 18);
+        map.put("shahana", 25);
+        map.put("saksham", 21);
+        map.put("saksham", 20);
 
 
 
