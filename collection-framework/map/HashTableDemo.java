fcb3a095610c24feea3658af5a0ec6c1874701a8
Shahana700 <shahanacs2003@gmail.com>
2025-07-20 21:04:00 +0530

    hashTable

---
+++ collection-framework/map/HashTableDemo.java
@@ -0,0 +1,16 @@
+import java.util.Hashtable;
+import java.util.Map;
+
+public class HashTableDemo {
+    public static void main(String[] args) {
+        Hashtable<Integer,String> s = new Hashtable<>();
+
+       s.put(1,"shahana");
+       s.put(1,"shahana");  
+       s.put(1,"shahana");
+       s.put(1,"shahana");
+
+    
+System.out.println(s);
+    }
+}
