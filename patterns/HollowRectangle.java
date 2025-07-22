c12ff800b26bf0472b1a25a96c8511d452ca8b44
Shahana700 <shahanacs2003@gmail.com>
2025-07-22 20:19:02 +0530

    a hollowRectangle

---
+++ patterns/HollowRectangle.java
@@ -0,0 +1,21 @@
+public class HollowRectangle {
+    public static void printHollowRectangle(int rows, int colums){
+        for(int i=1;i<=rows;i++){
+            for(int j=1;j<=colums;j++){
+                if(i==1 || i==rows || j==1 || j==colums){
+                    System.out.print("* ");
+            }else{
+                System.out.print("  ");
+            }
+
+        }
+        System.out.println();
+    }
+}
+public static void main(String args[]){
+    printHollowRectangle(5, 10);
+
+
+}
+}
+
