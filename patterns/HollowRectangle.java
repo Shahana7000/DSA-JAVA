aa1e4c186e1d456954d58272d23b99319ea19fdd
Shahana700 <shahanacs2003@gmail.com>
2025-07-22 21:55:56 +0530

    add a hollow triangle

--- patterns/HollowRectangle.java
+++ patterns/HollowRectangle.java
@@ -3,7 +3,7 @@ public class HollowRectangle {
         for(int i=1;i<=rows;i++){
             for(int j=1;j<=colums;j++){
                 if(i==1 || i==rows || j==1 || j==colums){
-                    System.out.print("* ");
+                    System.out.print(" *");
             }else{
                 System.out.print("  ");
             }
@@ -12,8 +12,24 @@ public class HollowRectangle {
         System.out.println();
     }
 }
+public static void Trianle(int n){
+    for(int i =1;i<=n;i++){
+        for(int s=1;s<=n-i;s++){
+            System.out.print(" ");
+        }
+        for(int j=1;j<=2*i-1;j++){
+            if(j==1||j==2*i-1|i==n){
+                System.out.print("*");
+            }else{
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+    }
+}
 public static void main(String args[]){
     printHollowRectangle(5, 10);
+    Trianle(5);
 
 
 }
