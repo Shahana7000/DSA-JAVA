5527f057eb2ee8b9bb54e11d8c90532ce6026730
Shahana700 <shahanacs2003@gmail.com>
2025-08-01 16:53:28 +0530

    numbers pyramid add

--- patterns/halfparamind.java
+++ patterns/halfparamind.java
@@ -10,7 +10,17 @@ public class halfparamind {
         System.out.println();
     }
 }
+public static void numberpyramid(int n){
+    for(int i = 1;i<=n;i++){
+        for(int j = 1;j<=n-i+1;j++){
+            System.out.print(j);
+        }
+         System.out.println();
+    }
+   
+}
     public static void main(String[] args) {
         paramint(5);
+        numberpyramid(5);
 }
     }
