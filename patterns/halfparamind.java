c7d53b5737b8c0ba20d46f66f0de7ccf901cd003
Shahana700 <shahanacs2003@gmail.com>
2025-08-01 18:29:58 +0530

    binary triangle

--- patterns/halfparamind.java
+++ patterns/halfparamind.java
@@ -29,9 +29,25 @@ public static void floyd(int n){
     System.out.println();
 }
 }
+public static void BinaryPyramid(int n){
+    for(int i = 1;i<=n;i++){
+        for(int j = 1;j<=i;j++){
+            if((i+j)%2==0){
+                System.out.print("1");
+            }
+            else{
+                System.out.print("0");
+        }
+        
+    
+}
+System.out.println();
+    }
+}
     public static void main(String[] args) {
         paramint(5);
         numberpyramid(5);
         floyd(5);
+        BinaryPyramid(5);
 }
     }
