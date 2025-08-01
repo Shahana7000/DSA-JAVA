200e2cd1922f07f13489e857473ca6aac908c14e
Shahana700 <shahanacs2003@gmail.com>
2025-08-01 17:36:15 +0530

    floyd pyramid

--- patterns/halfparamind.java
+++ patterns/halfparamind.java
@@ -19,8 +19,19 @@ public static void numberpyramid(int n){
     }
    
 }
+public static void floyd(int n){
+    int counter = 1;
+    for(int i =1;i<=n;i++){
+        for(int j = 1;j<=i;j++){
+            System.out.print(counter);
+            counter++;
+    }
+    System.out.println();
+}
+}
     public static void main(String[] args) {
         paramint(5);
         numberpyramid(5);
+        floyd(5);
 }
     }
