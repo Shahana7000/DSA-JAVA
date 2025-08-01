16a28e96f4bb3950fa12d4c057279755b52946ce
Shahana700 <shahanacs2003@gmail.com>
2025-08-01 16:39:56 +0530

    half-pyramid

--- patterns/HollowRectangle.java
+++ patterns/HollowRectangle.java
@@ -27,11 +27,29 @@ public static void Trianle(int n){
         System.out.println();
     }
 }
+public static void RevereseTriangle(int n){
+    for(int i=1;i<=n;i++){
+        for(int s=1;s<=n-i;s++){
+            System.out.print(" ");
+}
+for(int j=1;j<=2*i-1;j++){
+    if(j==1||j==2*i-1|i==n){
+        System.out.print("*");
+        }else{
+            System.out.print(" ");
+        }
+    System.out.println();
+}
+    }
+}
 public static void main(String args[]){
     printHollowRectangle(10, 10);
     Trianle(10);
+    RevereseTriangle(5);
 
 
 }
 }
+
+
 
--- patterns/patterns.java
+++ patterns/patterns.java
@@ -1,5 +1,3 @@
-package patterns;
-
 public class patterns {
     public static void main(String args[]){
         for(int i = 1; i<=5; i++){
