1ca3f183d376e44dba67b3ecd110339fb9d9666b
Shahana700 <shahanacs2003@gmail.com>
2025-08-02 23:02:41 +0530

    subarrays

---
+++ Array/ReverseArray.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class ReverseArray {
+    public static int convert(int[] numbers){
+        int start = 0, end = numbers.length-1;
+        while(start < end){
+            int temp = numbers[end];
+            numbers[end] = numbers[start];
+            numbers[start] = temp;
+            start ++;
+            end --;
+        }
+        return -1;
+
+    }
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        convert(numbers);
+        for(int i = 0; i < numbers.length; i++){
+            System.out.print(numbers[i] + " ");
+    
+    
+}
+}
+}
