646729af5d7c44b32b162777c3d933d325130276
Shahana700 <shahanacs2003@gmail.com>
2025-08-03 14:22:13 +0530

    follow new aproach - prefix

--- Array/MaxSubarraySum.java
+++ Array/MaxSubarraySum.java
@@ -2,15 +2,16 @@ public class MaxSubarraySum {
     public static void subarraySum(int[] numbers){
         int maxSum = Integer.MIN_VALUE;
         int curre = 0;
+        int prefix[] = new int[numbers.length];
+        for(int i =1;i<prefix.length;i++){
+            prefix[i] = prefix[i-1] + numbers[i];
+        }
         for(int i = 0;i<numbers.length;i++){
             int start = i;
             for(int j = i;j<numbers.length;j++){
                 int end = j;
-                curre = 0;
+                curre = start == 0 ? prefix[end] : prefix[end] - prefix[start-1];
 
-                for(int k = start;k<=end;k++){
-                    curre += numbers[k];
-                }
                 System.out.println(curre);
                 if(curre > maxSum){
                     maxSum = curre;
