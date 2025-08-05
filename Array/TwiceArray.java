9a77e687c65b6b3c16eb703cdf15e4e6b7444d66
Shahana700 <shahanacs2003@gmail.com>
2025-08-05 19:52:53 +0530

    target element return its index

--- Array/TwiceArray.java
+++ Array/TwiceArray.java
@@ -13,17 +13,34 @@ public class TwiceArray {
     public static void ArrayPair(int[] numbers){//array pairs
         for(int i = 0;i<numbers.length-1;i++){
             for(int j = i+1;j<numbers.length;j++){
+              
                 System.out.print("(" + numbers[i] + "," + numbers[j] + ") ");
+
             }
             System.out.println();
     }
 }
+public static int TargetArray(int[] numbers, int target){
+    for(int i =0;i<numbers.length;i++){
+        if(numbers[i] == target){
+            return i ;
+        }
+        
+    }
+    return -1;
+}
+    
+
+    
    
 
     public static void main(String args[]){
-        int[] numbers = {1,2,3,4,5,6,7,1,};
-        System.out.println(ArrayTwice(numbers));
-          ArrayPair(numbers);
+        // int[] numbers = {1,2,3,4,5,6,7,1,};
+        // System.out.println(ArrayTwice(numbers));
+        //   ArrayPair(numbers);
+        int[] numbers = {1,2,3,0};
+        System.out.println(TargetArray(numbers, 3));
+
     
 }
     }
