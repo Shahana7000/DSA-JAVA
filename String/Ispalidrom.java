831c17916cbb38eed4f2789c9b726e1f975fcf0e
Shahana700 <shahanacs2003@gmail.com>
2025-09-19 21:01:19 +0530

    string palindrom

---
+++ String/Ispalidrom.java
@@ -0,0 +1,18 @@
+public class Ispalidrom {
+    public static boolean isPalindrom(String str){
+        for(int i = 0;i<str.length()/2;i++){
+            int n = str.length();
+            if(str.charAt(i) != str.charAt(n-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String args[]){
+        String str = "moon" ;
+        System.out.println(isPalindrom(str));
+
+
+    }
+}
