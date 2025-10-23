cca4422577610c33145e3538745a79bb318707ff
Shahana7000 <shahanacs2003@gmail.com>
2025-10-23 14:54:01 +0530

    recursion

--- Recursion-Backtracking/Problem2.java
+++ Recursion-Backtracking/Problem2.java
@@ -2,17 +2,25 @@ public class Problem2 {
     public static void PrintReverseNum(int n){
 
         if(n==1){
-            System.out.println(n);
+            System.out.println(1);
             return;
         }
      System.out.print(n + " ");
         PrintReverseNum(n-1);
     }
 
-
+public static void PrinInc(int n){
+    if(n==1){
+        System.out.println(n +" ");
+        return ;
+    }
+    PrinInc(n-1);
+    System.out.println(n + " ");
+}
     public static void main(String[] args){
         int n = 10;
-        PrintReverseNum(n);
+        // PrintReverseNum(n);
+        PrinInc(n);
       
 
     }
